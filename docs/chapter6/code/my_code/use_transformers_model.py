81a989dcd604c1d443c16f2e22b643e47e30c9df
malele4th <malele19931203@gmail.com>
2026-05-15 10:54:58 +0800

    fix

--- docs/chapter6/code/my_code/use_transformers_model.py
+++ docs/chapter6/code/my_code/use_transformers_model.py
@@ -11,7 +11,7 @@ print(time.ctime(), "model 加载完成")
 
 
 messages = [
-    {"role": "user", "content": "你是谁? 你有哪些功能"},
+    {"role": "user", "content": "你是谁? 你有哪些功能？中国的首都在哪里"},
 ]
 print(time.ctime(), "输入 messages:", messages)
 
@@ -24,6 +24,6 @@ inputs = tokenizer.apply_chat_template(
 ).to(model.device)
 print(time.ctime(), "inputs 生成完成")
 
-outputs = model.generate(**inputs, max_new_tokens=256)
+outputs = model.generate(**inputs, max_new_tokens=40)
 print(time.ctime(), "推理结束, 输出:", tokenizer.decode(outputs[0][inputs["input_ids"].shape[-1]:]))
 
