bb1956e3fe3e776d2df95d4be082be67d317fbce
malele4th <malele19931203@gmail.com>
2026-05-26 10:45:08 +0800

    fix

--- docs/chapter6/code/local_train.sh
+++ docs/chapter6/code/local_train.sh
@@ -6,6 +6,7 @@ timestamp="$(date +%Y%m%d_%H%M%S)"
 pretrain_log_file="logs/pretrain_${timestamp}.log"
 sft_log_file="logs/sft_${timestamp}.log"
 lora_log_file="logs/lora_${timestamp}.log"
+dpo_log_file="logs/dpo_${timestamp}.log"
 
 # # 预训练
 # echo "$(date) [start pretrain]"
@@ -17,7 +18,11 @@ lora_log_file="logs/lora_${timestamp}.log"
 # nohup bash finetune.sh > ${sft_log_file} 2>&1 &
 # echo "$(date) [end sft]"
 
-echo "$(date) [start lora]"
-nohup bash pref_lora_sft.sh > ${lora_log_file} 2>&1 &
-echo "$(date) [end lora]"
+# echo "$(date) [start lora]"
+# nohup bash pref_lora_sft.sh > ${lora_log_file} 2>&1 &
+# echo "$(date) [end lora]"
+
+echo "$(date) [start dpo]"
+nohup bash pref_dpo.sh > ${dpo_log_file} 2>&1 &
+echo "$(date) [end dpo]"
 
